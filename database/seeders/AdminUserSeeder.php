bd0315e45b46c4f0113bff9f11165de6b7263772
Rian <riantarisagusti@gmail.com>
2026-06-13 11:28:34 +0700

    LAST

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -34,28 +34,29 @@ class AdminUserSeeder extends Seeder
                 'birth_date' => now()->subYears(20), // Today is birthday
             ]
         );
-        $customer1->assignRole('customer');
+    //     $customer1->assignRole('customer');
 
-        $customer2 = User::firstOrCreate(
-            ['email' => 'customer2@eeva.com'],
-            [
-                'name' => 'Member Aktif Biasa',
-                'password' => Hash::make('password'),
-                'phone' => '081234567892',
-                'member_until' => now()->addMonths(6),
-                'birth_date' => now()->subYears(25)->subMonths(3), // Not birthday
-            ]
-        );
-        $customer2->assignRole('customer');
+    //     $customer2 = User::firstOrCreate(
+    //         ['email' => 'customer2@eeva.com'],
+    //         [
+    //             'name' => 'Member Aktif Biasa',
+    //             'password' => Hash::make('password'),
+    //             'phone' => '081234567892',
+    //             'member_until' => now()->addMonths(6),
+    //             'birth_date' => now()->subYears(25)->subMonths(3), // Not birthday
+    //         ]
+    //     );
+    //     $customer2->assignRole('customer');
 
-        $customer3 = User::firstOrCreate(
-            ['email' => 'customer3@eeva.com'],
-            [
-                'name' => 'Bukan Member',
-                'password' => Hash::make('password'),
-                'phone' => '081234567893',
-            ]
-        );
-        $customer3->assignRole('customer');
-    }
+    //     $customer3 = User::firstOrCreate(
+    //         ['email' => 'customer3@eeva.com'],
+    //         [
+    //             'name' => 'Bukan Member',
+    //             'password' => Hash::make('password'),
+    //             'phone' => '081234567893',
+    //         ]
+    //     );
+    //     $customer3->assignRole('customer');
+    // }
+}
 }
